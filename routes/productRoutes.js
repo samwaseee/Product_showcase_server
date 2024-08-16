e8c2fabc5c6357bf40c42a380c97c953d1f81c99
samwaseee <samwaseee@gmail.com>
2024-08-16 20:52:18 +0600

    all filters added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,52 @@ const product = require('../models/product');
 
 const router = express.Router();
 
-router.get('/', async(req,res) =>{
-    const { sort, keyword, category } = req.query;
-    console.log(req.query)
-    const products = await product.find();
-    res.send(products);
+router.get('/', async (req, res) => {
+    const { sort, keyword, category, brand, priceValue } = req.query;
+    let sortBy = {};
+    let query = {};
+    // console.log(req.query)
+
+    switch (sort) {
+        case 'highToLow':
+            sortBy = { price: -1 };
+            break;
+        case 'lowToHigh':
+            sortBy = { price: 1 };
+            break;
+        case 'newestFirst':
+            sortBy = { creation_date: -1 };
+            break;
+    }
+
+    if (keyword) {
+        query.phone_name = { $regex: keyword, $options: 'i' };
+    }
+
+    if (category) {
+        query['category.name'] = category;
+    }
+    console.log(priceValue);
+
+    if (brand) {
+        query.brand = brand;
+    }
+
+    if (priceValue) {
+        // Ensure priceValue is an array and contains exactly two elements
+        const prices = Array.isArray(priceValue) ? priceValue.map(Number) : [];
+        if (prices.length === 2 && !isNaN(prices[0]) && !isNaN(prices[1])) {
+            const [minPrice, maxPrice] = prices;
+            query.price = { $gte: minPrice, $lte: maxPrice };
+        }
+    }
+
+    try {
+        const products = await product.find(query).sort(sortBy);
+        res.send(products);
+    } catch (err) {
+        res.status(500).send({ error: 'Failed to fetch products' });
+    }
 })
 
 
