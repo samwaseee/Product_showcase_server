b369780d0b426261b30a1c355b03b97a35936f00
samwaseee <samwaseee@gmail.com>
2024-08-16 12:32:45 +0600

    frontend input checked

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async(req,res) =>{
     const { sort, keyword, category } = req.query;
-    // console.log(req.query)
+    console.log(req.query)
     const products = await product.find();
     res.send(products);
 })
